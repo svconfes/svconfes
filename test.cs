91f596558baf8df48d100dbe882e01ee5caaabeb
Ngoc Pham <tuanngocptn@users.noreply.github.com>
2017-11-24 02:00:08 +0700

    Update test.cs
    1

--- test.cs
+++ test.cs
@@ -1,29 +1,19 @@
 $(document).ready(function () {
-    $("#btn-login").click(function () {
-        ajaxcall('api/user', {}, 'get', checklogin);
+    $("#btn-sign-up").click(function(){
+        var user = {};
+        user.Name = $("#name").val();
+        user.Phone = $("#phone").val();
+        user.Password = $("#pass").val();
+        ajaxcall("api/user", user, "post", checkSignup)
     });
-    if (localStorage.getItem('user') != null) {
-        if (JSON.parse(localStorage.getItem('user')).RoleCode == 'adm') {
-            redirect("order.html")
-        }
-        if (JSON.parse(localStorage.getItem('user')).RoleCode == 'ctm') {
-            redirect("product.html")
-        }
-    }
 });
 
-function checklogin(data) {
-    data.forEach(function (element) {
-        var user = $("#user").val();
-        var pass = $("#pass").val();
-        if ((user == element.Code || user == element.Phone) && pass == element.Password) {
-            localStorage.setItem("user", JSON.stringify(element));
-            if(element.RoleCode == 'ctm'){
-                redirect("product.html")
-            }else{
-                redirect("order.html")
-            }
-        }
-    }, this);
-    $(".alert-danger").removeClass("hidden");
+function checkSignup(data){
+    if(data.Status){
+        $(".alert-success").removeClass("hidden");
+        $(".alert-danger").attr('class', 'alert alert-danger hidden');
+    }else{
+        $(".alert-danger").removeClass("hidden");
+        $(".alert-success").attr('class', 'alert alert-success hidden');
+    }
 }
