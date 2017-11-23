e7ef75e66850effd568d04936d41176811f12111
Ngoc Pham <tuanngocptn@users.noreply.github.com>
2017-11-24 01:55:28 +0700

    Update test.cs
    2

--- test.cs
+++ test.cs
@@ -1,40 +1,39 @@
-function ajaxcall(url, value, method, callback) {
-    $.ajax({
-        url: constants.server + url, 
-        type: method,
-        data: value,
-        cache: false,
-        dataType: "json",
-        crossOrigin: false,
-        success: function (result) {
-            callback(result);
-        },
-        false:function(result){
-            callback(result);
-        }
-    });
+$(document).ready(function () {
+    loadCartTable();
+});
+
+var total = 0;
+
+function loadCartTable(){
+    var cart = JSON.parse(localStorage.getItem('cart'));
+    $(".tbl-cart-body").empty();
+    cart.forEach(function(element) {
+        $(".tbl-cart-body").append(getRow(element));
+    }, this);
+    $(".tbl-cart-sum").text(total);
 }
 
-function ajaxput(url, value, method, callback){
-    $.ajax({
-        url: constants.server + url, 
-        type: "POST",
-        data: value,
-        contentType : "application/json",
-        cache: false,
-        dataType: "json",
-        crossOrigin: false,
-        headers: {
-            "X-HTTP-Method-Override": method
-        },
-        success: function (result) {
-            callback(result);
-        },
-        false:function(result){
-            callback(result);
-        }
-    });
+function getRow(element){
+    total = total + element.Price * element.Quantity;
+    return '<tr>'
+            +'<td>'+ element.Code +'</td>'
+            +'<td>'+ element.Name +'</td>'
+            +'<td>'+ element.Price +'</td>'
+            +'<td>'+ element.Quantity +'</td>'
+            +'<td>'+ element.Price * element.Quantity +'</td>'
+        +'</tr>'
 }
 
-jQuery.support.cors = true;
-crossOrigin: false;
+function cancelCart(){
+    localStorage.setItem("cart", JSON.stringify([]));
+    redirect("product.html");
+}
+
+function addOrder(){
+    var order = {};
+    order.CodeUser = JSON.parse(localStorage.getItem('user')).Code;
+    order.Products = JSON.parse(localStorage.getItem('cart'));
+    ajaxcall('api/order', order, 'post', function(){});
+    localStorage.setItem("cart", JSON.stringify([]));
+    redirect("order.html");
+}
