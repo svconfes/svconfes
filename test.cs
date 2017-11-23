449a6e06c494bddeb9e3a0191ea673141bce7189
Ngoc Pham <tuanngocptn@users.noreply.github.com>
2017-11-24 02:03:55 +0700

    Update test.cs
    1

--- test.cs
+++ test.cs
@@ -1,64 +1,105 @@
-$(document).ready(function () {
-    ajaxcall("api/user", {}, "get", loadUser);
+$(document).ready(function(){
+    ajaxcall("api/order", {}, "get", loadOrder);
 });
 
-function loadUser(data) {
-    $(".tbl-user").empty();
-    data.forEach(function (element) {
-        $(".tbl-user").append(getRowUser(element));
+var user = {};
+
+function loadOrder(data){
+    $(".tbl-order").empty();
+    user = JSON.parse(localStorage.getItem('user'));
+    data.forEach(function(element) {
+        if(user.RoleCode == 'ctm'){
+            if(user.Code == element.CodeUser){
+                $(".tbl-order").append(ctmRole(element));
+            }
+        }
+        if(user.RoleCode == 'adm'){
+            $(".tbl-order").append(adminRole(element));
+        }
     }, this);
 }
 
-function getRowUser(element) {
-    console.log(element);
+function adminRole(element){
+    return (element.CodeStatus == 'working' ? `<tr class="warning">` : element.CodeStatus == 'done' ? `<tr class="success">` : `<tr class="danger">`)
+        +`<td>`+ element.Ord +`</td>`
+        +`<td>`+ element.Code +`</td>`
+        +`<td>`+ element.CodeUser +`</td>`
+        +`<td>`+ element.CodeStatus +`</td>`
+        +`<td>`+ element.Price +`</td>`
+        +`<td>`
+        +`<a href="#"  data-toggle="modal" data-target="#myModal" data-order='`+ JSON.stringify(element) +`' onclick="loadCartTable(this)">`
+            +`<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>`
+        +`</a>` 
+        +`</td>`
+        +`<td>`
+        + (element.CodeStatus == 'working' ? 
+            `<a href="#" data-order='`+ JSON.stringify(element) +`' onclick="donelOrder(this)">`
+                +`<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>`
+            +`</a>` : '')
+        +`</td>`
+        +`<td>`
+        + (element.CodeStatus == 'working' ? 
+            `<a href="#" data-order='`+ JSON.stringify(element) +`' onclick="cancelOrder(this)">`
+                +`<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>`
+            +`</a>` : '')
+        +`</td>`
+        +`</tr>`;
+}
+
+function ctmRole(element){
+    return (element.CodeStatus == 'working' ? `<tr class="warning">` : element.CodeStatus == 'done' ? `<tr class="success">` : `<tr class="danger">`)
+        +`<td>`+ element.Ord +`</td>`
+        +`<td>`+ element.Code +`</td>`
+        +`<td>`+ element.CodeUser +`</td>`
+        +`<td>`+ element.CodeStatus +`</td>`
+        +`<td>`+ element.Price +`</td>`
+        +`<td>`
+        +`<a href="#"  data-toggle="modal" data-target="#myModal" data-order='`+ JSON.stringify(element) +`' onclick="loadCartTable(this)">`
+            +`<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>`
+        +`</a>` 
+        +`</td>`
+        +`<td>`
+        + (element.CodeStatus == 'working' ? 
+            `<a href="#" data-order='`+ JSON.stringify(element) +`' onclick="cancelOrder(this)">`
+                +`<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>`
+            +`</a>` : '')
+        +`</td>`
+        +`<td>`+ `` +`</td>`
+        +`</tr>`;
+}
+
+function cancelOrder(element){
+    var order = $(element).data("order");
+    order.CodeStatus = 'cancelled'
+    ajaxcall("api/order/5", order, "PUT", function(){});
+    location.reload();
+}
+
+function donelOrder(element){
+    var order = $(element).data("order");
+    order.CodeStatus = 'done'
+    ajaxcall("api/order/5", order, "PUT", function(){});
+    location.reload();
+}
+
+var total = 0;
+function loadCartTable(element){
+    total = 0
+    $(".tbl-cart-body").empty();
+    var order = $(element).data("order");
+    (order.Products).forEach(function(element) {
+        $(".tbl-cart-body").append(getRow(element));
+    }, this);
+    $(".tbl-cart-sum").text(total);
+}
+
+function getRow(element){
+    total = total + element.Price * element.Quantity;
     return '<tr>'
-        + '<td>' + element.Ord + '</td>'
-        + '<td>' + element.Code + '</td>'
-        + '<td>' + element.Name + '</td>'
-        + '<td>' + element.Password + '</td>'
-        + '<td>' + element.RoleCode + '</td>'
-        + '<td>' + element.Phone + '</td>'
-        + `<td><a class="btn btn-primary" data-toggle="modal" data-target="#myModal" onclick="editUser(this)" data-element='` + JSON.stringify(element) + `'><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></></td>`
-        + `<td><a class="btn btn-danger" href="#" onclick="deleteUser('` + element.Code + `')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>`
-        + '</tr>';
-}
-
-function deleteUser(code) {
-    var value = { "Code": code };
-    ajaxcall("api/user/5", value, "delete", showPopup);
-}
-
-function showPopup(bool) {
-    if (bool) {
-        location.reload();
-    } else {
-        $(".alert-danger").removeClass("hidden");
-    }
-}
-
-function adduser() {
-    var user = {};
-    user.Code = $("#User-code").val();
-    user.Name = $("#User-name").val();
-    ajaxcall("api/user", user, "post", location.reload());
-}
-
-function editUser(element){
-    element = JSON.parse($(element).attr('data-element'));
-    $("#user-code").val(element.Code);
-    $("#user-role").val(element.RoleCode);
-    $("#user-name").val(element.Name);
-    $("#user-password").val(element.Password);
-    $("#user-phone").val(element.Phone);
-    console.log(element);
-}
-
-function doUpdateUser(){
-    var user = {};
-    user.Code = $("#user-code").val();
-    user.RoleCode = $("#user-role").val();
-    user.Name = $("#user-name").val();
-    user.Password = $("#user-password").val();
-    user.Phone = $("#user-phone").val();
-    ajaxcall("api/user/5", user, "put", location.reload());
+            +'<td>'+ element.Code +'</td>'
+            +'<td>'+ element.Name +'</td>'
+            +'<td>'+ element.Price +'</td>'
+            +'<td>'+ element.Quantity +'</td>'
+            +'<td>'+ element.Price * element.Quantity +'</td>'
+        +'</tr>'
 }
