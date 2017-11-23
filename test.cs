82c60f27004e06bfa214a9caad7270e090b8427e
Ngoc Pham <tuanngocptn@users.noreply.github.com>
2017-11-23 23:46:44 +0700

    Update test.cs
    t

--- test.cs
+++ test.cs
@@ -1,4 +1,3 @@
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,98 +9,129 @@ using webappdev.entities;
 
 namespace webappdev.Controllers
 {
-    public class CategoryController : ApiController
+    public class OrderController : ApiController
     {
         WebAppDevDBDataContext webAppDevDBDataContext;
-        public CategoryController()
+        public OrderController()
         {
             webAppDevDBDataContext = new WebAppDevDBDataContext();
         }
-
-        // GET api/category
-        public IEnumerable<Category> Get()
+        // GET api/order
+        public IEnumerable<Order> Get()
         {
-            List<TblCategory> tblCategorys = webAppDevDBDataContext.TblCategories.ToList();
-            List<Category> lstCategory = new List<Category>();
-            foreach (TblCategory tblCategory in tblCategorys)
+            List<TblOrder> tblOrders = webAppDevDBDataContext.TblOrders.OrderByDescending(x => x._ord).ToList();
+            List<Order> lstOrder = new List<Order>();
+            foreach (TblOrder tblOrder in tblOrders)
             {
-                Category category = new Category();
-                category.Ord = tblCategory._ord;
-                category.Code = tblCategory._code;
-                category.Name = tblCategory._name;
-                lstCategory.Add(category);
+                Order order = new Order();
+                order.Ord = tblOrder._ord;
+                order.Code = tblOrder._code;
+                order.CodeUser = tblOrder._codeUser;
+                order.CodeStatus = tblOrder._codeStatus;
+                order.Price = (long)tblOrder._price;
+                order.Products = getLstProductDetailByCode(order.Code);
+                lstOrder.Add(order);
             }
-            return lstCategory;
+            return lstOrder;
         }
 
-        // GET api/category/5
+        // GET api/order/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/category
-        public void Post([FromBody]Category category)
+        // POST api/order
+        public void Post([FromBody]Order order)
         {
-            if (category != null && category.Code != null && category.Name != null)
+            TblOrder tblOrder = new TblOrder();
+            tblOrder._codeStatus = "working";
+            tblOrder._codeUser = order.CodeUser;
+            tblOrder._code = "ord" + (webAppDevDBDataContext.TblOrders.OrderBy(c => c._ord).ToList().Last()._ord + 1);
+            long price = 0;
+            foreach (Product product in order.Products)
             {
-                TblCategory tblCategory = new TblCategory();
-                tblCategory._name = category.Name;
-                tblCategory._code = category.Code;
-                webAppDevDBDataContext.TblCategories.InsertOnSubmit(tblCategory);
-                try
-                {
-                    webAppDevDBDataContext.SubmitChanges();
-                }
-                catch (Exception) { }
+                TblOrderDetail tblOderDetail = new TblOrderDetail();
+                tblOderDetail._codeOrder = tblOrder._code;
+                tblOderDetail._codeProduct = product.Code;
+                tblOderDetail._quantity = product.Quantity;
+                price = product.Price * product.Quantity + price;
+                webAppDevDBDataContext.TblOrderDetails.InsertOnSubmit(tblOderDetail);
+            }
+            tblOrder._price = price;
+            webAppDevDBDataContext.TblOrders.InsertOnSubmit(tblOrder);
+            webAppDevDBDataContext.SubmitChanges();
+        }
+
+        // PUT api/order/5
+        public void Put([FromBody]Order order)
+        {
+            if (order != null)
+            {
+                var values = from items in webAppDevDBDataContext.TblOrders
+                             where items._code == order.Code
+                             select items;
+                values.First()._codeStatus = order.CodeStatus;
+                webAppDevDBDataContext.SubmitChanges();
+                return;
             }
         }
 
-        // PUT api/category/5
-        public void Put([FromBody]Category category)
+        // DELETE api/order/5
+        public void Delete([FromBody]Order order)
         {
-            if (category != null && category.Ord != null && category.Code != null && category.Name != null)
+            if (order != null && order.CodeStatus == "working")
             {
-                var values = from items in webAppDevDBDataContext.TblCategories
-                             where items._ord == category.Ord
+                var values = from items in webAppDevDBDataContext.TblOrders
+                             where items._code == order.Code
                              select items;
                 if (values.Count() < 1)
                 {
                     return;
                 }
-                foreach (TblCategory tblCategory in values)
+                webAppDevDBDataContext.TblOrders.DeleteOnSubmit(values.First());
+
+                var valueDetails = from items in webAppDevDBDataContext.TblOrderDetails
+                         where items._codeOrder == order.Code
+                         select items;
+
+                foreach (TblOrderDetail tblOrderDetail in valueDetails)
                 {
-                    tblCategory._name = category.Name;
-                    break;
+                    webAppDevDBDataContext.TblOrderDetails.DeleteOnSubmit(tblOrderDetail);
                 }
                 webAppDevDBDataContext.SubmitChanges();
             }
         }
 
-        // DELETE api/category/5
-        public bool Delete([FromBody]Category category)
+        private List<Product> getLstProductDetailByCode(string codeOrder)
         {
-            var values = from items in webAppDevDBDataContext.TblCategories
-                         where items._ord == category.Ord
-                         select items;
-            if (values.Count() < 1)
-            {
-                return false;
+            List<TblOrderDetail> tblOrderDetails = webAppDevDBDataContext.TblOrderDetails.ToList();
+            List<TblProduct> tblProducts = webAppDevDBDataContext.TblProducts.ToList();
+            List<Product> lstProduct = new List<Product>();
+
+            foreach(TblOrderDetail tblOrderDetail in tblOrderDetails){
+                if (tblOrderDetail._codeOrder == codeOrder)
+                {
+                    foreach (TblProduct tblProduct in tblProducts)
+                    {
+                        if (tblProduct._code == tblOrderDetail._codeProduct)
+                        {
+                            Product product = new Product();
+                            product.Ord = tblProduct._ord;
+                            product.Code = tblProduct._code;
+                            product.Name = tblProduct._name;
+                            product.CodeCategory = tblProduct._codeCategory;
+                            product.Price = (long)tblProduct._price;
+                            product.Quantity = (int)tblProduct._quantity;
+                            product.Detail = tblProduct._detail;
+                            product.UrlImage = tblProduct._urlImage;
+                            product.IsAds = (bool)tblProduct._isAds;
+                            lstProduct.Add(product);
+                        }
+                    }
+                }
             }
-            foreach (TblCategory tblCategory in values)
-            {
-                webAppDevDBDataContext.TblCategories.DeleteOnSubmit(tblCategory);
-                break;
-            }
-            try
-            {
-                webAppDevDBDataContext.SubmitChanges();
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
+            return lstProduct;
         }
     }
 }
